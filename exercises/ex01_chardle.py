c49116dd0d403c224324476a0fcccd08e5efc575
Yuan Yao <yaoyuan@unc.edu>
2023-09-11 23:27:06 -0400

    1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,19 +2,19 @@
 __author__ = "730713746"
 x: str = input("Enter a 5-character word:")
 if len(x) != 5:
-    exit("Word must contain 5 characters")
+    exit("Error: Word must contain 5 characters")
 y: str = input("Enter a single character:")
 if len(y) != 1:
-    exit("Character must be a single character")
-print("Searching for", y ,"in" ,x)
+    exit("Error: Character must be a single character")
+print("Searching for", y, "in", x)
 index: int = 0
 times: int = 0
 for letter in x:
     index = index+1
     if letter == y:
-        print(y, "found at index",index)
+        print(y, "found at index", index)
         times = times+1
 if times == 0:
-    print ("No instances of", y ,"found in", x)
+    print ("No instance of", y, "found in", x)
 else:
-    print(times, "instances of", y, "found in", x)
+    print(times, "instance of", y, "found in", x)
