bca4cd68a417bb229e08b59d2baf216dc7a85435
Yuan Yao <yaoyuan@unc.edu>
2023-10-17 11:44:50 -0400

    1

---
+++ lessons/sum.py
@@ -0,0 +1,20 @@
+def w_sum(x: list[float]) -> float:
+    index = 0
+    outcome = 0.0
+    while index < len(x):
+        outcome += x[index]
+    return outcome
+
+
+def f_sum(x: list[float]) -> float:
+    outcome = 0.0
+    for i in x:
+        outcome += i
+    return outcome
+
+
+def f_ranage_sum(x: list[float]) -> float:
+    outcome = 0.0
+    for i in (0,len(x),1):
+        outcome += x[i]
+    return outcome
