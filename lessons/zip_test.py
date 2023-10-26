d003e0da9300315b3fcaee3a0b2b7653c174a143
Yuan Yao <yaoyuan@unc.edu>
2023-10-26 11:44:56 -0400

    1

--- lessons/zip_test.py
+++ lessons/zip_test.py
@@ -5,20 +5,20 @@ from lessons.zip import zip
 
 def test_empty_list() -> None:
     """Test the output of two empty lists."""
-    x: list = []
-    y: list = []
-    assert zip (x, y) == {}
+    x: list[str] = []
+    y: list[int] = []
+    assert zip(x, y) == {}
 
 
 def test_different_lenth() -> None:
-    """Test the output of lists with differet len"""
-    x: list = ["a", "b", "c"]
-    y: list = [1, 2]
-    assert zip (x, y) == {}
+    """Test the output of lists with differet len."""
+    x: list[str] = ["a", "b", "c"]
+    y: list[int] = [1, 2]
+    assert zip(x, y) == {}
 
 
 def test_usage() -> None:
-    """Test the output of main usage"""
-    x: list = ["a", "b", "c"]
-    y: list = [1, 2, 3]
-    assert zip (x, y) == {"a": 1, "b": 2, "c": 3}
+    """Test the output of main usage."""
+    x: list[str] = ["a", "b", "c"]
+    y: list[int] = [1, 2, 3]
+    assert zip(x, y) == {"a": 1, "b": 2, "c": 3}
