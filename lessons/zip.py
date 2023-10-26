cf8bdc24dd7b2b501cec149a605f46614df71262
Yuan Yao <yaoyuan@unc.edu>
2023-10-26 10:40:21 -0400

    1

--- lessons/zip.py
+++ lessons/zip.py
@@ -4,7 +4,7 @@ __author__ = "730713746"
 
 def zip(x: list[str], y: list[int]) -> dict[str, int]:
     """Combining two lists into a dictionary."""
-    output: dict[str, int] = ()
+    output: dict[str, int] = {}
     idx = 0
     if len(x) != len(y) or len(x) == 0 or len(y) == 0:
         return output
