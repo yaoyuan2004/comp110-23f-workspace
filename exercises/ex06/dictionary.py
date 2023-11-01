b2ee3a9d4c4ccdb71bb8005a006bdd83f6307ba7
Yuan Yao <yaoyuan@unc.edu>
2023-11-01 17:25:02 -0400

    1

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -1,8 +1,9 @@
 """Combining two lists into a dictionary."""
 __author__ = "730713746"
 
-def invert(x: dict[str: str]) -> dict[str: str]:
-    """Exchange key and value."""
+
+def invert(x: dict[str, str]) -> dict[str, str]:
+    """It would exchange key and value."""
     y: dict[str, str] = {}
     for i in x:
         y[x[i]] = i
@@ -11,11 +12,11 @@ def invert(x: dict[str: str]) -> dict[str: str]:
     return y
 
 
-def favorite_colors(x: dict[str: str]) -> str:
-    """Find highest frequency."""
-    bank: dict[str: int] ={}
+def favorite_colors(x: dict[str, str]) -> str:
+    """It would find highest frequency."""
+    bank: dict[str, int] = {}
     for i in x:
-        if (x[i] in bank) == False:
+        if (x[i] in bank) is False:
             bank[x[i]] = 1
         else:
             bank[x[i]] += 1
@@ -29,10 +30,10 @@ def favorite_colors(x: dict[str: str]) -> str:
 
 
 def count(x: list[str]) -> dict[str: int]:
-    """Count all frequency."""
-    num: dict[str: int] = {}
+    """It would count all frequency."""
+    num: dict[str, int] = {}
     for i in x:
-        if (i in num) == False:
+        if (i in num) is False:
             num[i] = 1
         else:
             num[i] += 1
@@ -40,10 +41,10 @@ def count(x: list[str]) -> dict[str: int]:
 
 
 def alphabetizer(x: list[str]) -> dict[str, list[str]]:
-    """Count all frequency."""
+    """It woule like to count all frequency with capital."""
     num: dict[str, list[str]] = {}
     for i in x:
-        if (i[0] in num) == False:
+        if (i[0] in num) is False:
             num[i[0]] = [i]
         else:
             num[i[0]].append(i)
@@ -52,7 +53,7 @@ def alphabetizer(x: list[str]) -> dict[str, list[str]]:
 
 def update_attendance(x: dict[str, list[str]], y: str, z: str) -> dict[str, list[str]]:
     """Update the new dictionary."""
-    if (y in x) == False:
+    if (y in x) is False:
         x[y] = [z]
     else:
         x[y].append(z)
