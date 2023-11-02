c7b28043eccda995019fbe13b4b39b938fe4cef6
Yuan Yao <yaoyuan@unc.edu>
2023-11-02 13:10:49 -0400

    1

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -7,21 +7,25 @@ class Point:
     """Creat the class points."""
     x: float
     y: float
+    
 
     def __init__(self, x_init: float, y_init: float):
+        """Construction of the init."""
         self.x = x_init
         self.y = y_init
 
 
     def scale_by(self, factor: int):
+        """Construction of the scale_by."""
         self.x = self.x * factor
         self.y = self.y * factor
 
     
     def scale(self, factor: int) -> Point:
+        """Construction of the scale."""
         x = self.x * factor 
         y = self.y * factor
-        a: Point = (x, y)
+        a: Point = Point(x, y)
         return a
 
 
